8eff42d3762e1693a9f8ec5ec5ce8745a0ea97a6
2001karim-dot <karimpenuel17@gmail.com>
2025-03-31 13:20:07 +0300

    first commitss

--- firstaid_chatbot/settings.py
+++ firstaid_chatbot/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-r2xj1z3+gid&7_=a=qnkgq2m#*)z=2^uley@%ci#6uyu#9gx_&
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,20 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# This setting informs Django of the URI path from which your static files will be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
 
-if DEBUG:  # Allow static files in development
-   STATICFILES_DIRS = [
-    BASE_DIR / "chatbot/static",
-]
-else:  # Ensure static files are served in production
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
